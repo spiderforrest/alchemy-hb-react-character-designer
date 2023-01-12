16b6bb35a4431738467313fecc064dea8b6511b7
spiderforrest <spider@spood.org>
2023-01-12 11:24:07 -0800

    header added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Main from './components/Main.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
     </div>
   );
